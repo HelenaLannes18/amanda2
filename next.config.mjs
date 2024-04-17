6ce16cfb978cd4dcfe23a14bcdf794f83d8c28a2
HelenaLannes18 <helena.lannes.salles@hotmail.com>
2024-04-16 21:23:49 -0300

    enviando 5

--- next.config.mjs
+++ next.config.mjs
@@ -1,37 +1,6 @@
-const { createProxyMiddleware } = require('http-proxy-middleware');
-/**
- * @type {import('next').NextConfig}
- */
-module.exports = {
-  images: {
-    domains: [
-      'res.cloudinary.com',
-      'abs.twimg.com',
-      'pbs.twimg.com',
-      'avatars.githubusercontent.com',
-    ],
-  },
-  api: {
-    bodyParser: {
-      sizeLimit: '5mb', // ou o tamanho desejado
-    },
-  },
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   reactStrictMode: true,
-  swcMinify: false, // Required to fix: https://nextjs.org/docs/messages/failed-loading-swc
-  async rewrites() {
-    return [
-      {
-        source: '/api/auth/google',
-        destination: 'https://accounts.google.com',
-      },
-    ];
-  },
-  async middleware() {
-    const proxyMiddleware = createProxyMiddleware('/api/auth/google', {
-      target: 'https://accounts.google.com',
-      changeOrigin: true,
-    });
-
-    return [proxyMiddleware];
-  },
 };
+
+export default nextConfig;
